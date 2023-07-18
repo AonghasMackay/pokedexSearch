2d6b2c85f60ebc26a168dfa24b2804102864075e
AonghasMackay <aonghasmackay@yahoo.co.uk>
2023-07-19 00:26:17 +0100

    update handler with completed fillPokemonData function

--- modules/pokemonHandler.js
+++ modules/pokemonHandler.js
@@ -14,16 +14,96 @@ function fillPokemonData(pokemonJSON, documentFragment) {
     const statsCollapse = createStatsCollapse(pokemonJSON);
     documentFragment.querySelector('#pokemonDetails').appendChild(statsCollapse);
 
+    const weightElem = createWeightElem(pokemonJSON);
+    documentFragment.querySelector('#pokemonDetails').appendChild(weightElem);
+
+    const heightElem = createHeightElem(pokemonJSON);
+    documentFragment.querySelector('#pokemonDetails').appendChild(heightElem);
+
+    const typesElem = createTypesElem(pokemonJSON);
+    documentFragment.querySelector('#pokemonDetails').appendChild(typesElem);
+
+    const movesCollapse = createMovesCollapse(pokemonJSON);
+    documentFragment.querySelector('#pokemonDetails').appendChild(movesCollapse);
+
+    const appearsInCollapse = createAppearsInCollapse(pokemonJSON);
+    documentFragment.querySelector('#pokemonDetails').appendChild(appearsInCollapse);
+
     console.log(pokemonJSON);
 
     return documentFragment;
+}
+
+function createAppearsInCollapse(pokemonJSON) {
+    const detailsCollapseTemplate = document.querySelector('#pokemonDetailsCollapseTemplate');
+
+    let appearsInCollapse = detailsCollapseTemplate.content.cloneNode(true);
+    appearsInCollapse.querySelector('#pokemonDetailsCollapseSummary').innerHTML = '<strong>Appears in</strong>';
+
+    pokemonJSON.game_indices.forEach(game => {
+        const gameNode = document.createElement('span');
+        gameNode.innerHTML = `${game.version.name}, `;
+
+        appearsInCollapse.querySelector('#pokemonDetailsCollapseContent').appendChild(gameNode);
+    });
+
+    return appearsInCollapse;
+}
+
+function createHeightElem(pokemonJSON) {
+    const heightElem = document.createElement('span');
+    const pokemonHeight = pokemonJSON.height / 10;
+
+    heightElem.innerHTML = `<strong>Height:</strong> ${pokemonHeight}m <br />`;
+
+    return heightElem;
+}
+
+function createMovesCollapse(pokemonJSON) {
+    const detailsCollapseTemplate = document.querySelector('#pokemonDetailsCollapseTemplate');
+
+    let movesCollapse = detailsCollapseTemplate.content.cloneNode(true);
+    movesCollapse.querySelector('#pokemonDetailsCollapseSummary').innerHTML = '<strong>Moves</strong>';
+
+    pokemonJSON.moves.forEach(move => {
+        const moveNode = document.createElement('span');
+        moveNode.innerHTML = `${move.move.name}, `;
+
+        movesCollapse.querySelector('#pokemonDetailsCollapseContent').appendChild(moveNode);
+    });
+
+    return movesCollapse;
+}
+
+function createTypesElem(pokemonJSON) {
+    const typesElem = document.createElement('span');
+    typesElem.innerHTML = '<strong>Types:</strong> ';
+
+    pokemonJSON.types.forEach(type => {
+        typesElem.innerHTML += `${type.type.name}, `;
+    });
+    //remove trailing comma
+    typesElem.innerHTML = typesElem.innerHTML.slice(0, -2);
+
+    typesElem.innerHTML += '<br />';
+
+    return typesElem;
+}
+
+function createWeightElem(pokemonJSON) {
+    const weightElem = document.createElement('span');
+    const pokemonWeight = pokemonJSON.weight / 10;
+
+    weightElem.innerHTML = `<strong>Weight:</strong> ${pokemonWeight}kg <br />`;
+
+    return weightElem;
 }
 
 function createStatsCollapse(pokemonJSON) {
     const detailsCollapseTemplate = document.querySelector('#pokemonDetailsCollapseTemplate');
 
     let statsCollapse = detailsCollapseTemplate.content.cloneNode(true);
-    statsCollapse.querySelector('#pokemonDetailsCollapseSummary').innerHTML = 'Stats';
+    statsCollapse.querySelector('#pokemonDetailsCollapseSummary').innerHTML = '<strong>Stats</strong>';
 
     pokemonJSON.stats.forEach(stat => {
         const statNode = document.createElement('span');
